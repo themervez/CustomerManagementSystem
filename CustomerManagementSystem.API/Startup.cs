3b63eebecac7f6ddac66b2f90e593cb45d4a321c
Merve ÖZ <104096244+themervez@users.noreply.github.com>
2023-08-08 14:58:58 +0300

    API processes

--- CustomerManagementSystem.API/Startup.cs
+++ CustomerManagementSystem.API/Startup.cs
@@ -1,7 +1,16 @@
 using CustomerManagementSystem.Core.Configuration;
+using CustomerManagementSystem.Core.Repositories;
+using CustomerManagementSystem.Core.Services;
+using CustomerManagementSystem.Core.UnitOfWork;
+using CustomerManagementSystem.Data.DbContexts;
+using CustomerManagementSystem.Data.Repositories;
+using CustomerManagementSystem.Data;
+using CustomerManagementSystem.Service.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,6 +21,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CustomerManagementSystem.Core.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace CustomerManagementSystem.API
 {
@@ -27,9 +38,56 @@ namespace CustomerManagementSystem.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Will set as DI Container later
+
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped(typeof(IGenericService<,>), typeof(GenericService<,>));//*
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SqlServer"), sqlOptions =>
+                {
+                    sqlOptions.MigrationsAssembly("CustomerManagementSystem.Data");//for migrations in data layer
+                });
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>(opt =>
+            {
+                opt.User.RequireUniqueEmail = true;
+                opt.Password.RequireNonAlphanumeric = false;//it can change
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();//when using in passwaord reset 
+
 
             services.Configure<CustomTokenOption>(Configuration.GetSection("TokenOption"));
             services.Configure<List<Client>>(Configuration.GetSection("Clients"));//Options Pattern
+
+            services.AddAuthentication(options => //for identity verification
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;//1 Authentication schema
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;//for communicate each other
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opts =>
+            {
+                var tokenOptions = Configuration.GetSection("TokenOption").Get<CustomTokenOption>();
+
+                opts.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    //validation steps,rules
+                    ValidIssuer = tokenOptions.Issuer,
+                    ValidAudience = tokenOptions.Audience[0],//0
+                    IssuerSigningKey = SignService.GetSymmetricSecurityKey(tokenOptions.SecurityKey),//signature validation
+
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero//Token's control for zones tolerate,can change
+                };
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -49,7 +107,7 @@ namespace CustomerManagementSystem.API
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
